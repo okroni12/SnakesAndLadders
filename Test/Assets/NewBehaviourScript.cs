924296bf9afbdf0e93d50fc9a26dc92e2e23a81d
okroni12 <okroni12@student.aau.dk>
2016-11-24 12:30:53 +0100

    adedededededed scrpt

---
+++ Test/Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 10; i++)
+        {
+            print(i);
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
